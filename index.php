392eab0819385a291cfe663678141eba4c3f1769
zRdriyw <78458395+zRdriyw@users.noreply.github.com>
2023-11-25 13:44:39 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rodrigo's portfolio</title>
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+    <header>
+        <img class="pfp" src="./src/pfp.jpg" alt="Rodrigo's photo."> <br>
+        <div class="break-line-main"><div class="break-line"></div></div>
+        Rodrigo Esteban Inostroza Garrido
+        <h5>17 Años - 24/05/2006 - Valdivia, Chile</h5>
+    </header>
+    <nav>
+        <ul>
+		    <li><a href="#">About Me</a></li>
+		    <li><a href="#">Contact</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Twitter</a></li>
+	    </ul>
+    </nav>
+</body>
+</html>
